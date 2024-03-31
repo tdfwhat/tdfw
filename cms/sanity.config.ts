e930af4685962e8f75fef3846b453803fe1f5369
antonpalsson <antonemanuel@pm.me>
2024-03-31 18:59:46 +0200

    Init

---
+++ cms/sanity.config.ts
@@ -0,0 +1,21 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'tdfw',
+
+  projectId: 'o8bcoztu',
+  dataset: 'production',
+
+  plugins: [
+    structureTool(), 
+    visionTool()
+  ],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
---
+++ cms/schemaTypes/index.ts
@@ -0,0 +1,14 @@
+export const schemaTypes = [
+  {
+    title: "Test",
+    name: "test",
+    type: "document",
+    fields: [
+      {
+        title: "Greeting",
+        name: "greeting",
+        type: "string"
+      }
+    ]
+  }  
+]
